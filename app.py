9fce9b27b430526e3509ac80f9ae4a4c12a17de5
Ket Patel <ketz555@hotmail.com>
2023-10-19 16:53:17 +0100

    change in message to test CD with separate branch

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world! It's raining :("
+    return "Hello, world! Testing CD with a separate branch"
 
 if __name__ == '__main__':
     app.run(
