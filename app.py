6aa95f023f4329f1e3b086da677c9be75bed3653
Ket Patel <ketz555@hotmail.com>
2023-10-19 14:24:03 +0100

    changed message to test CD

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world! It's raining :("
+    return "Hello, world! It's sunny :)"
 
 if __name__ == '__main__':
     app.run(
