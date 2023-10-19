f6171bbbda1e657f48ec4a1da937950e5970ed53
Ket Patel <ketz555@hotmail.com>
2023-10-19 11:45:47 +0100

    Fix something

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world! make this test fail"
+    return "Hello, world! It's raining :("
 
 if __name__ == '__main__':
     app.run(
